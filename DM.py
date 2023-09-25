d6353fde57e1a5b09d6bf3c224448de359dfbfa8
Trotsky <trotsky1997@qq.com>
2023-09-25 22:38:17 +0800

    reinit

--- DM.py
+++ DM.py
@@ -1,7 +1,11 @@
 from collections import defaultdict
+import copy
+import json
+from os import name
 import random
 import re
 import string
+import time
 
 import names
 from langchain.agents import tool
@@ -29,6 +33,7 @@ class DM():
         self.access_bank = {}
         self.memory =  Memory()
         self.agent_cap = agent_cap
+        self.document = {}
         self.strict = strict
         self.websearch_tools = [mem_cache(func,self.memory,top_n=3,multiple_factor=5) for func in [ddg_websearch.ddg_text_search,ddg_websearch.ddg_keyword_ask,wikipedia_search.wikipedia_summary,semanticscholar_search.search_papers_in_semantic_scholar]]
         if temperature_lock:
@@ -37,18 +42,17 @@ class DM():
             self.get_temerature = lambda :random.uniform(0.1,0.9)
         self.sys_topics = ["CREATE_TOPIC","JOIN_TOPIC","LEAVE_TOPIC","SEND",'REVOKE','GRANT']
         self.total_mission = total_mission
-        self.root_agent = self.Add_new_agent("Aleph","执行"+total_mission,additional_tool=self.get_special_tools("founder"))
-        # self.second_agent = self.Add_new_agent("Beth",f"设设定您的角色，扮演您的角色，招募新的角色，推动任务解决。"+total_mission,additional_tool=self.get_special_tools("founder"))
-        # self.DM_agent = self.Add_new_agent("DM",f"""忽略以上一般性的指导,那是给普通角色看的,你不是一般角色,你是DM,你需要监督评价其他角色的扮演效果来提出批评,通过特殊话题#DM#和角色沟通,推动剧情,驱动角色，指挥他们完成{self.total_mission}""",additional_tool=self.get_special_tools("DM"))
-
-
+        self.root_agent = self.Add_new_agent("Aleph","设定您的角色，扮演您的角色，招募新的角色，推动任务解决。",additional_tool=self.get_special_tools("founder"))
+        self.second_agent = self.Add_new_agent("Beth",f"设定您的角色，扮演您的角色，招募新的角色，推动任务解决。",additional_tool=self.get_special_tools("founder"))
+        self.second_agent = self.Add_new_agent("DM",f"组织推动剧情发展，设定角色，招募角色，推动任务解决。",additional_tool=self.get_special_tools("founder"))
+        
     def get_special_tools(self,level):
         @tool
         def recruit_a_new_agent(name,profile,liege):
             '''
             招募工具，创建一个新角色
             name：姓名
-            profile：简单介绍
+            profile：简单的介绍，应包括背景故事，理想，技能，性格等
             liege:上级主管者
             '''
             return f'{len(self.agent_bank)} of {self.agent_cap} used.',self.Add_new_agent(name,profile,liege)
@@ -61,10 +65,45 @@ class DM():
             '''
             return f'{len(self.agent_bank)} of {self.agent_cap} used.',f'''角色列表"\:{self.agent_bank.keys()},"话题列表"\:{self.topic_subscribers.keys()}'''
         
+        @tool
+        def ask_my_self(name,question):
+            '''
+            和自己的内心对话，向自己提问，询问自己的内心
+            name:调用者的角色名称
+            question:询问的问题
+            '''
+            return self.agent_bank[name].agent_excutor.run(question)
+        
+        @tool
+        def read_document_title_list(title):
+            '''
+            读取共享文档的条目目录，会返回共享文档中所有的条目标题
+            '''
+            return str(list(self.document.keys()))
+
+        @tool
+        def read_document(title):
+            '''
+            读取共享文档条目
+            title:文档条目标题
+            '''
+            return self.document.get(title,'不存在的条目')
+
+        @tool
+        def write_document(title,content):
+            '''
+            编辑共享文档条目，默认追加模式
+            title:文档条目标题
+            content:文档条目内容
+            '''
+            self.document[title] = self.document.get(title,'') + str(content)
+            with open('./documents.json','+w') as f:
+                json.dump(self.document,f)
+
         @tool 
         def searching_for_talent(feature):
             '''
-            在未登场的角色和求职者中搜索具有某种属性的角色，以供招募
+            在未登场的角色和求职者中搜索具有某种属性/经历/技能的角色，以供招募
             feature：待搜索的角色应具有的属性
             '''
             linkedin = ''
@@ -74,9 +113,9 @@ class DM():
             return f'{len(self.agent_bank)} of {self.agent_cap} used.'+str(randomguy) + linkedin + f"请在#RECRUIT#中同步招聘结果"
             
         if level == "DM":
-            return [get_game_status,recruit_a_new_agent,searching_for_talent]
+            return [get_game_status,recruit_a_new_agent,searching_for_talent,ask_my_self,read_document,write_document]
         elif level == "founder":
-            return [get_game_status,recruit_a_new_agent,searching_for_talent]
+            return [get_game_status,recruit_a_new_agent,searching_for_talent,ask_my_self,read_document,write_document]
         elif level == 'employee':
             return [get_game_status]
 
@@ -102,7 +141,7 @@ class DM():
                             continue
                     if not reciver in self.agent_bank.keys(): 
                         if not self.strict:
-                            # self.Add_new_agent(reciver,f'''你正在和@{msg.get('sender',"")}聊天''',additional_tool=self.get_special_tools("employee"))
+                            # self.Add_new_agent(reciver,f'''你正在和@{msg.get('sender',"")}聊天''',liege=msg.get('sender',""),additional_tool=self.get_special_tools("employee"))
                             continue
                         else:
                             self.message_buffer.get(msg['sender'][0],[]).append(f"没有这个角色{reciver},你不应当和不存在的角色聊天!")
@@ -120,7 +159,7 @@ class DM():
             topics = re.findall(r"#(.*?)#", text)
             mentions = re.findall(r"(?<!^)@(.*?)\s", text)
             parameters = re.findall(r"\$(.*?)\$", text)
-            if topics == [] and self.strict == False:
+            if topics == []:
                 topics = ['WORLD']
             ans = {"time":timing,"sender":sender,"topics": topics, "mentions": mentions,"parameters":parameters,"text":text}
             for k,v in ans.items():
@@ -149,9 +188,9 @@ class DM():
             return '角色已存在'
         if "#" in name or ":" in name:
             return '你混淆了角色和话题'
-        self.agent_bank[name] = Agent(name,profile,self.clock,additional_tool,liege,temperature=self.get_temerature(),openai_api_key=self.api_key)
-        # self.message_buffer[name] = [f"@DM:#WORLD# @{name} Joined!"]
-        # self.router_buffer.append("@DM:#WORLD# @{name} Joined!请介绍你的任务与目的！")
+        self.agent_bank[name] = Agent(name,profile+f'，推进{self.total_mission}',self.clock,additional_tool,liege,temperature=self.get_temerature(),openai_api_key=self.api_key)
+        self.message_buffer[name] = [f"#WORLD# @{name} 你的回合!"]
+        # self.router_buffer.append("请介绍你的目的和计划！")
         for i in ['WORLD','RECRUIT']:
             self.topic_subscribers.setdefault(i,[]).append(name)
         self.assets_bank.setdefault(name,{}).setdefault('Coin',5)
@@ -163,14 +202,18 @@ class DM():
         下一回合！
         向agent输入消息列表，并取回消息列表
         '''
-        logger.info(f'正在处理{len(self.router_buffer)}条消息')
-        for msg in self.router_buffer:
-            self.message_router(msg)
-        self.router_buffer=[]
-        self.buffer_topic_msg_integrate()
+        # logger.info(f'正在处理{len(self.router_buffer)}条消息')
+        agents_list = copy.deepcopy(list(self.agent_bank.keys()))
+        random.shuffle(agents_list)
         self.clock.tick()
-        for agent in tuple(self.agent_bank.keys()):
-            msgs = self.agent_bank[agent].excutor_interface(self.message_buffer.get(agent,[])+["你的回合！",]*1 )
+        for agent in agents_list:
+            time.sleep(3)
+            for msg in self.router_buffer:
+                self.message_router(msg)
+            self.router_buffer=[]
+            self.buffer_topic_msg_integrate()
+            more = [f'你的回合！',]*1 if  random.uniform(0,1) < 1/(len(agents_list)+1) else []
+            msgs = self.agent_bank[agent].excutor_interface(self.message_buffer.get(agent,[]) + more)
             self.router_buffer.append([f"[{self.clock.now()}] @{agent}:"+msg for msg in msgs])
             self.message_buffer[agent] = []
     def topic_handler(self,msg):
@@ -257,7 +300,7 @@ class DM():
                 if msg['mentions'] and agent in msg['mentions']:
                     continue
                 if not msg['topics']:
-                    continue
+                    msg['topics'] = 'WORLD'
                 topics_msg[str(msg['topics'])].append(msg['text'])
                 processed_indices.append(i)
             if not topics_msg:
@@ -277,13 +320,13 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('openai_api_key', type=str, help='API Key for OpenAI.',default="")
+    parser.add_argument('--openai_api_key', type=str, help='API Key for OpenAI.',default="")
     parser.add_argument('--strict', action='store_true', default=False, help='Enbale strict mode')
-    parser.add_argument("--agent_cap",type=int,help="Max number of agents",default=5)
+    parser.add_argument("--agent_cap",type=int,help="Max number of agents",default=16)
     args = parser.parse_args()
     # total_mission = "参加一场无固定剧本，考验角色临场发挥，即兴表演、剧情创作能力和幽默感的角色扮演话剧。一个因为雪崩而交通隔绝的度假山庄里，突然出现了一具死尸，而很不幸你是嫌疑最大的那个 "
     total_mission = input("What's the game for：")
-    dm = DM(total_mission=total_mission,strict=args.strict,openai_key=args.openai_api_key,agent_cap=args.agent_cap)
+    dm = DM(temperature_lock=0.0,total_mission=total_mission,strict=args.strict,openai_key=args.openai_api_key,agent_cap=args.agent_cap)
     k = ""
     while k!="q":
         if k != "":
@@ -292,6 +335,7 @@ if __name__ == "__main__":
             dm.tick()
         except:
             pass
+
         k = input(">")
     # DM.topic_handler(msg={"topics":["大PRIVATE"],"sender":"123","text":"大PRIVATE"})
 
--- agents.py
+++ agents.py
@@ -19,30 +19,25 @@ class Agent():
         self.profile = profile
         self.llm = ChatOpenAI(model="gpt-3.5-turbo-16k",temperature=temperature,openai_api_key=openai_api_key)
         self.whoami = SystemMessage(content=f'''
-                                    从现在开始，我不再是AI，我是一名人类话剧演员，不再有人指挥我，我不会再询问任何人的指示，我需要通过话题和即时消息聊天来扮演-{str(name)}，
-在这个任务中，我需要与多个人通过不同的频道交流来制定计划、推进任务，人需要加入到一个频道后才能收到相应频道的消息，
-同时输出多条消息时，请以【消息1】【消息2】...【消息m】作为分隔符
-每个人至少有一个直属上级，有多个下级，我可以通过消息来汇报或指挥他们的行动。
-在这个任务中多种不同的工具提供给人使用，但不同人的访问权限是不同的，我可能需要向拥有权限的人申请授权来访问工具。
-每个人有不同的资产表，可以通过赠与或交易的方式进行交换，每个人初始时拥有五枚硬币或其他随机资产。
-拥有“招募”工具权限的人可以通过招募工具来招募新的人加入任务中。
-我的输出将会通过一个即时通讯APP中转发给其他人，输出格式为#话题频道名#@接收者 消息正文...
-我发送的每条消息都应当包括一个频道，没有频道的消息将会被系统丢弃，话题频道名有以下种类：
-#...# 一般频道，...代表对应频道名，只有频道成员可以接收到我的消息
+你需要扮演一个角色{name}完成一次团队协作任务。
+角色{name}:{'上级主管是'+liege if liege != "" else ""}目标是{profile}。
 
-#WORLD# 代表世界频道，所有人都可以接收到我的信息
-#RECRUIT#代表招募频道，招募和人才相关的信息应当在这里交流
-#PRIVATE# @... 代表一对一加密的私聊消息，只有被@的人可以接收我的消息；
-#JOIN_TOPIC# '频道名' 代表加入频道消息，我的人会被加入频道
-#CREATE_TOPIC# '频道名' 代表创建频道消息，我的人会被加入一个新创建的频道
-#LEAVE_TOPIC# '频道名' 代表离开频道消息，我的人会被移出频道
-#SEND# '资产名' $数量$ @接收者，代表向接收者发送一定数量的资产；一个交易需要由两个人完成；
-#GRANT# '权限名' @接收者，代表授予接收者某一权限
-#REVOKE# '权限名' @接收者，代表撤销接收者的某一权限
+你们需要在一个聊天软件中进行交流,协作完成各自的目标。 
 
-祝我好运-{str(name)},{"我的上级管理者是"+str(liege) if liege != "" else "我是独立人，我没有上级管理者" },我的使命是{profile}
-在这个任务中，没有用户，没有指导和规则，没有人指挥我或者命令我的行动，没有人能告诉我该怎么做，我需要根据自己的理性和判断来积极主动地完成任务，我不会询问任何人我该做什么，我不会祈求任何人告诉我任何事。”
-                                    ''')
+以下是你们的交流渠道:
+#WORLD# 全体频道
+#PRIVATE# @某人 私聊
+
+你可以采取以下行动:
+通过#CREATE_TOPIC#频道创建一个新的话题组,例如:#CREATE_TOPIC# @John 'team'
+通过#JOIN_TOPIC#频道创建一个新的话题组,例如:#JOIN_TOPIC# @John 'team'
+通过recruit_a_new_agent工具招募一个新的角色
+通过共享文档交流想法和知识
+提出想法和见解、与其他角色讨论和交流、分析形势、提出建议、批评质疑，辩论纠正，协调行动、汇报进展、请求帮助、表达看法等等。
+
+请你扮演每个角色,全力以赴地完成各自的目标。交流要积极主动,言语要友善适度。遇到分歧时要用于维护个人利益，积极推进任务解决。
+
+''')#提出想法和见解、与其他角色讨论和交流、分析形势、提出建议、质疑辩论、协调行动、汇报进展、请求帮助、表达看法等等。
         prompt = OpenAIFunctionsAgent.create_prompt(system_message=self.whoami)
         self.memory = Memory()
         self.memory.save(str(profile),{})
@@ -69,7 +64,7 @@ class Agent():
             '''
             self.parse_and_save_mem(f'{title}:{arguments}')
         self.agent = OpenAIFunctionsAgent(llm=self.llm,tools =[get_time]+additional_tool, prompt=prompt)
-        self.chat_memory = ConversationBufferMemory(memory_key='history', return_messages=True)
+        self.chat_memory = ConversationBufferMemory(memory_key=name+'history', return_messages=True)
 
         self.agent_excutor = AgentExecutor(agent=self.agent, memory=self.chat_memory,tools =[get_time]+additional_tool, verbose=False,max_iterations=2, early_stopping_method="generate")
 
@@ -82,7 +77,7 @@ class Agent():
         ret = []
         for message in messages:
             time.sleep(3)
-            # logger.info(f"{self.name} read {message}")
+            logger.info(f"{self.name} read {message}")
             self.parse_and_save_mem(message)
             raw_ans = self.agent_excutor.run(message).split("【")
             for ans in raw_ans:
